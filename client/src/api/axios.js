799d43635004015ddb24d906d3fcb41e09e7df45
/mr-Narasimma-the-developer <nsimma407@gmail.com>
2026-07-29 22:24:18 +0530

    Fix production API URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL =
+  import.meta.env.VITE_API_URL ||
+  "http://localhost:5000";
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
